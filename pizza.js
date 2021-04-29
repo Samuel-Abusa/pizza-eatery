cdca2da4ea43510cf8f37e06c38240887811cdb4
Samuel-Abusa <samuelabusakennedy@gmail.com>
2021-04-29 18:32:22 +0300

    Few changes on the styling and Javascript parts

--- pizza.js
+++ pizza.js
@@ -1,3 +1,63 @@
 $ (document).ready(function (){
   $(".prices").hide();
 });
+
+function showOrder (price, piz){
+  price.show();
+  piz.hide();
+}
+
+$ (".place1").click(function(){
+  showOrder($(".price1"), $(".piz1"));
+});
+
+$ (".place2").click(function(){
+  showOrder($(".price2"), $(".piz2"));
+})
+
+$ (".place3").click(function(){
+  showOrder($(".price3"), $(".piz3"));
+});
+
+$ (".form1").submit(function (event){
+  event.preventDefault();
+
+  var priceOfPizz1 = 10;
+  var noOfPizz1 = $ ("#amnt1").val();
+
+  function addCrust(a){
+    return +a;
+  }
+  
+
+
+
+
+
+
+  /*function addCrust(a){
+    return (priceOfPizz1 * noOfPizz1) + (a * noOfPizz1);
+  };*/
+  
+
+
+
+
+
+
+
+  /*var crusty = function (){
+    if ($ ("#crust1").val() == "Crispy"){
+      alert(addCrust(2));
+    }else{
+      if($ ("#crust1").val() == "Stuffed"){
+        alert(addCrust(3));
+      }else{
+        alert(addCrust(4));
+      }
+    }
+  }
+  function addT(){
+    + 
+  }*/
+});
